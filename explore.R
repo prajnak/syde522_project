21bcd19b096482849dbc2e23f67b33a7134c467a
Prajna Kandarpa <prajna.kandarpa@uwaterloo.ca>
2016-04-08 04:11:40 -0400

    finally found a reliable data source for yield data

--- explore.R
+++ explore.R
@@ -1,6 +1,9 @@
 library(Quandl)
 library(ggplot2)
+library(dplyr)
+library(readr)
 
+library(WDI)
 
 area_principal_crops_rice = Quandl("MOSPI/AREA_PRINCPL_CROPS_8_2_RICE",
                                    api_key="Q_zK1Gdy59wWp8Bkh9wH")
@@ -13,3 +16,46 @@ avg_yield_rice_1 = Quandl("MOSPI/AVG_YLD_PRINCPL_CROPS_8_4_RICE", api_key="Q_zK1
 summary(avg_yield_rice_1)
 
 f=ggplot(data=avg_yield_rice_1, aes(Year, Rice))
+f + geom_line()
+
+## read in the really big agricultural climate dataset
+
+raw_dat = read.table('rawdata/india60.cfm', sep=' ', header = F)
+rdf = reshape(raw_dat, 227, 271, direction='long')
+res <- lapply(raw_dat, function(ch) grep("[A-Z]+", ch))
+
+dim(res[res != 0])
+
+var_names = read.csv('rawdata/vars.txt', sep=' ', header=T)
+
+FUN <- function(x) {
+    x <- as.integer(x)
+    div <- seq_len(abs(x))
+    factors <- div[x %% div == 0L]
+    factors <- list(neg = -factors, pos = factors)
+    return(factors)
+}
+
+raw_dat = readLines('rawdata/india75.cfm')
+
+## install packages to read data from the world bank dataset
+install.packages('rWBclimate')
+install.packages('WDI')
+
+WDIsearch('rainfall')
+
+df_cereal = data.frame()
+
+cereal_yield = WDI(indicator = 'AG.YLD.CREL.KG', country = c('IN'), start = 1960, end = 2013)
+
+cereal_land = WDI(indicator = 'AG.LND.CREL.HA', country = c('IN'), start = 1960, end = 2013)
+head(cereal_land)
+
+f = ggplot(data = cereal_yield, aes(year, AG.YLD.CREL.KG))
+f+geom_line() + ggtitle('Cereal Yield in India from 1960-2013')
+
+df_cereal = data.frame(cereal_land$AG.LND.CREL.HA)
+colnames(df_cereal) = c('land')
+df_cereal$year = cereal_yield$year
+df_cereal$yield = cereal_yield$AG.YLD.CREL.KG
+
