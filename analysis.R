38a93b92546240052d4c1b5666c8a4f3b1819c62
Prajna Kandarpa <prajna.kandarpa@uwaterloo.ca>
2016-04-21 17:27:25 -0400

    divide up into two versions

---
+++ analysis.R
@@ -0,0 +1,10 @@
+library(dplyr)
+library(ggplot2)
+
+library(caret)
+library(AppliedPredicticeModelling)
+
+
+load('df_Cereal.RData') # loads the saved data from disk into the df_cereal data frame object
+
+df_cereal 
--- explore.R
+++ explore.R
@@ -6,6 +6,9 @@ library(readr)
 library(WDI)
 library(rWBclimate)
 
+library(caret)
+library(AppliedPredictiveModeling)
+
 area_principal_crops_rice = Quandl("MOSPI/AREA_PRINCPL_CROPS_8_2_RICE",
                                    api_key="Q_zK1Gdy59wWp8Bkh9wH")
 
@@ -40,9 +43,10 @@ FUN <- function(x) {
 raw_dat = readLines('rawdata/india75.cfm')
 
 ## install packages to read data from the world bank dataset
-install.packages('rWBclimate')
-install.packages('WDI')
+# install.packages('rWBclimate')
+# install.packages('WDI')
 
+countries = c('IN')
 WDIsearch('maize')
 ## read in data for cereal yield
 cereal_yield = WDI(indicator = 'AG.YLD.CREL.KG', country = c('IN'),
@@ -50,8 +54,11 @@ cereal_yield = WDI(indicator = 'AG.YLD.CREL.KG', country = c('IN'),
 cereal_land = WDI(indicator = 'AG.LND.CREL.HA', country = c('IN'),
                   start = 1960, end = 2013)
 head(cereal_land)
+agri_machinery_value = WDI(indicator = 'NV.MNF.MTRN.ZS.UN', country=c('IN'), start=1960)
+
 
 table(WDI(indicator = 'AG.CRP.MZE.CD')$country)
+agri_machinery_value = WDI(indicator = 'NV.MNF.MTRN.ZS.UN', country=c('IN'), start=1960, end=2013)
 
 f = ggplot(data = cereal_yield, aes(year, AG.YLD.CREL.KG))
 f+geom_line() + ggtitle('Cereal Yield in India from 1960-2013')
@@ -61,4 +68,38 @@ df_cereal = data.frame(cereal_land$AG.LND.CREL.HA)
 colnames(df_cereal) = c('land')
 df_cereal$year = cereal_yield$year
 df_cereal$yield = cereal_yield$AG.YLD.CREL.KG
+df_cereal$machines = agri_machinery_value$NV.MNF.MTRN.ZS.UN
 
+Asia_country
+Asia_basin
+get_historical_temp(c("IND"), "year")
+
+options(kmlpath = '~/.kmltemp')
+as_basin = create_map_df(Asia_basin)
+
+WDIsearch('loans')
+fert_usage = WDI(indicator = "AG.CON.FERT.MT", country = countries, start=1960, end=2013)
+
+df_cereal$fertilizer = fert_usage$AG.CON.FERT.ZS
+
+WDI(indicator = "AG.CON.FERT.PT.ZS", country = countries, start=1960, end=2013)
+
+ggplot(as_basin, aes(x=long, y=lat, group=group)) + geom_polygon()
+
+
+temps = get_historical_temp(c('IND'), time_scale = "year")
+temps = arrange(temps, -row_number())
+temps_annual = temps$data[1:54]
+df_cereal$temps = temps_annual
+
+precip = get_historical_precip(c("IND"), "year")
+precip = arrange(precip, -row_number())
+precip_annual = precip$data[1:54]
+df_cereal$rain = precip_annual
+
+pairs(df_cereal)
+WDIsearch('irrigation')
+methane_emission = WDI(indicator = 'EN.ATM.METH.AG.KT.CE', country=c('IN'), start=1960, end=2013)
+df_cereal$methane = methane_emission$EN.ATM.METH.AG.KT.CE
+
+save(df_cereal, file='df_Cereal.RData')
