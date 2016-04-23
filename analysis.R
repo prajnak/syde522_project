7e2756c85b16faa9b8784f66c6d420bac93b4739
Prajna Kandarpa <prajna.kandarpa@uwaterloo.ca>
2016-04-23 19:06:16 -0400

    analysis done. going ahead with the report

--- analysis.R
+++ analysis.R
@@ -12,27 +12,20 @@ load('df_Cereal.RData') # loads the saved data from disk into the df_cereal data
 
 ## get rid of columns with NA values
 cols_NA = unlist(lapply(df_cereal[1:53,], function(x) any(is.na(x))))
-##    land     year    yield machines    temps     rain  methane 
-##    FALSE    FALSE   FALSE TRUE        FALSE     FALSE     TRUE 
-predsArray = c(1,4,5);
-df_noNA = df_cereal[1:53,-c(4,7)]
-featurePlot(x=df_noNA[,predsArray], y=df_noNA$yield, plot='line', auto.key=list(columns=3))
 
-decades = c('60s', '70s', '80s', '90s', '00s', '10s')
+df_noNA = df_cereal[1:53,-c(4,5)]
+
 nearZeroVar(df_noNA, saveMetrics = T)
 
+predsArray = c(1,2,4,5,6,7)
 df_predictors = df_noNA[, predsArray]
 df_yield = df_noNA[, 3]
 
-train_idx = createDataPartition(y=df_noNA$yield, p=0.95)
+train_idx = createDataPartition(y=df_yield, p=0.70)
 pred_train = df_predictors[train_idx$Resample1, ]
 yield_train = df_yield[train_idx$Resample1]
 pred_test = df_predictors[-train_idx$Resample1, ]
 yield_test = df_yield[-train_idx$Resample1]
-
-
-
-
 
 ## Using Caret's averaged neural network model to make the predictions
 preproc_arguments = c('center', 'scale')
@@ -46,7 +39,6 @@ avNNetModel = train(x=pred_train, y=yield_train, trControl = fitControl,
                     method="avNNet", preProc=preproc_arguments,
                     linout=TRUE,trace=FALSE,
                     MaxNWts=40 * (ncol(pred_train)+1) + 10 + 1, maxit=1000)
-
 avNNetPred = predict(avNNetModel, newdata=pred_train)
 avNNetPR = postResample(pred=avNNetPred, obs=yield_train)
 rmses_training = c(avNNetPR[1])
@@ -56,21 +48,95 @@ avnnetPred_all = predict(avNNetModel, newdata=df_predictors)
 avnnetPR_all = postResample(pred=avnnetPred_all, obs = df_yield)
 rmses_testing = c(avnnetPR_all[1])
 r2s_testing = c(avnnetPR_all[2])
+quartz()
 ggplot(avNNetModel, metric='Rsquared')
 out_name = paste("nnet_pred_obs", as.numeric(as.POSIXct(Sys.time())), ".png")
 trellis.device(device="png", width=8, height=5, units="in", filename=out_name, res=100)
 print(plot(df_noNA$year, df_noNA$yield, type='l', col=2))
 print(lines(df_noNA$year, avnnetPred_all, col=3))
-print(legend(x='topright',c('actual', 'predicted'), fill=2:3))
+print(legend(x='topleft',c('actual', 'predicted'), fill=2:3))
 dev.off()
 
 ggplot(avNNetModel, metric="Rsquared") + ggtitle("Performance of average neural network model")        
 ggsave("avgnnet_rsqr.png", width=8, height=5, units="in", dpi=100)
 
+library(doMC)
+registerDoMC(cores=4)
+
+## Multiple adaptive Regressive Splines (MARS)
+marsGrid = expand.grid(.degree=1:2, .nprune=2:38)
+
+marsModel = train(x=pred_train, y=yield_train,
+                  trControl = fitControl, preProcess = preproc_arguments,
+                  method = "earth", tuneLength = 20,
+                   tuneGrid = marsGrid)
+anfisPreds = predict(marsModel, newdata=pred_test)
+
+res = as.data.frame(cbind(pred_test$year, yield_test, anfisPreds))
+res = melt(res, id.vars = "V1")
+quartz()
+ggplot(marsModel, metric="Rsquared")
+quartz()
+ggplot(res, aes(V1,value,col=variable)) + geom_line() + ggtitle("Validation performance for MARS model")
+marsPred_all = predict(marsModel, newdata=df_predictors)
+res = as.data.frame(cbind(df_predictors$year, df_yield, marsPred_all))
+res = melt(res, id.vars ="V1")
+quartz()
+ggplot(res, aes(V1, value, col=variable)) + geom_line() + ggtitle("Training + Validation Data combined performance for MARS")
+
+## Neural network with multiple layers
+paramGrid <- expand.grid(.layer1 = c(10,9,8,7), .layer2 = c(6,5,4), .layer3 = c(1,2,3));
+nnetModel = train(x=pred_train, y=yield_train, trControl = fitControl,
+                  preProcess=preproc_arguments, method="neuralnet",
+                  tuneLength=5, tuneGrid = paramGrid)
+
+nnet_preds = predict(nnetModel, newdata=pred_test)
+res = as.data.frame(cbind(pred_test$year, yield_test, nnet_preds ))
+colnames(res) = c("year", "actual", "nnet_prediction")
+res = melt(res, id.vars = "V1")
+quartz()
+ggplot(nnetModel, metric="Rsquared")
+quartz()
+ggplot(res, aes(V1, value, col=variable)) + geom_line() + ggtitle("Validation performance for ML NN")
+
+
 
 
 ## install.packages('forecast')
 library(neuralnet)
+trellis.par.set(caretTheme())
+## nnet = neuralnet()
+library(rpart)
+library(randomForest)
 
-nnet = neuralnet()
-# convert to timeseries
+## Use decision trees
+fit_dectree <- rpart(yield ~ land + temps + rain, method="anova", data=df_noNA)
+quartz()
+plot(df_noNA$year, df_noNA$yield, type='l', col=2)
+lines(df_noNA$year, fit_dectree$predicted, col=3)
+legend(x='topright', c('actual', 'predicted', fill=2:3))
+
+
+fit <- randomForest(yield ~ land + temps + rain, data=df_noNA, proximity = T)
+df_noNA$predicted_rfs = fit$predicted
+
+plotcp(fit)
+printcp(fit)
+summary(fit)
+plot(fit)
+
+library(reshape2)
+
+d <- melt(df_noNA[,2:7], id.vars='year')
+quartz()
+ggplot(d, aes(year, value, col=variable)) + geom_line() + scale_y_log10() +scale_x_continuous(breaks = seq(1961,2012, by=5)) + geom_text(data=df_noNA[df_noNA$year==2012,],aes(label=))
+       
+
+
+
+
+
+
+
+
+
--- explore.R
+++ explore.R
@@ -9,38 +9,38 @@ library(rWBclimate)
 library(caret)
 library(AppliedPredictiveModeling)
 
-area_principal_crops_rice = Quandl("MOSPI/AREA_PRINCPL_CROPS_8_2_RICE",
-                                   api_key="Q_zK1Gdy59wWp8Bkh9wH")
+## area_principal_crops_rice = Quandl("MOSPI/AREA_PRINCPL_CROPS_8_2_RICE",
+##                                    api_key="Q_zK1Gdy59wWp8Bkh9wH")
 
-head(area_principal_crops_rice)
-f = ggplot(data=area_principal_crops_rice, aes(Year, Rice))
-f + geom_line()
+## head(area_principal_crops_rice)
+## f = ggplot(data=area_principal_crops_rice, aes(Year, Rice))
+## f + geom_line()
 
-avg_yield_rice_1 = Quandl("MOSPI/AVG_YLD_PRINCPL_CROPS_8_4_RICE", api_key="Q_zK1Gdy59wWp8Bkh9wH")
-summary(avg_yield_rice_1)
+## avg_yield_rice_1 = Quandl("MOSPI/AVG_YLD_PRINCPL_CROPS_8_4_RICE", api_key="Q_zK1Gdy59wWp8Bkh9wH")
+## summary(avg_yield_rice_1)
 
-f=ggplot(data=avg_yield_rice_1, aes(Year, Rice))
-f + geom_line()
+## f=ggplot(data=avg_yield_rice_1, aes(Year, Rice))
+## f + geom_line()
 
-## read in the really big agricultural climate dataset
+## ## read in the really big agricultural climate dataset
 
-raw_dat = read.table('rawdata/india60.cfm', sep=' ', header = F)
-rdf = reshape(raw_dat, 227, 271, direction='long')
-res <- lapply(raw_dat, function(ch) grep("[A-Z]+", ch))
+## raw_dat = read.table('rawdata/india60.cfm', sep=' ', header = F)
+## rdf = reshape(raw_dat, 227, 271, direction='long')
+## res <- lapply(raw_dat, function(ch) grep("[A-Z]+", ch))
 
-dim(res[res != 0])
+## dim(res[res != 0])
 
-var_names = read.csv('rawdata/vars.txt', sep=' ', header=T)
+## var_names = read.csv('rawdata/vars.txt', sep=' ', header=T)
 
-FUN <- function(x) {
-    x <- as.integer(x)
-    div <- seq_len(abs(x))
-    factors <- div[x %% div == 0L]
-    factors <- list(neg = -factors, pos = factors)
-    return(factors)
-}
+## FUN <- function(x) {
+##     x <- as.integer(x)
+##     div <- seq_len(abs(x))
+##     factors <- div[x %% div == 0L]
+##     factors <- list(neg = -factors, pos = factors)
+##     return(factors)
+## }
 
-raw_dat = readLines('rawdata/india75.cfm')
+## raw_dat = readLines('rawdata/india75.cfm')
 
 ## install packages to read data from the world bank dataset
 # install.packages('rWBclimate')
@@ -70,36 +70,76 @@ df_cereal$year = cereal_yield$year
 df_cereal$yield = cereal_yield$AG.YLD.CREL.KG
 df_cereal$machines = agri_machinery_value$NV.MNF.MTRN.ZS.UN
 
-Asia_country
-Asia_basin
-get_historical_temp(c("IND"), "year")
+## Asia_country
+## Asia_basin
+## get_historical_temp(c("IND"), "year")
 
-options(kmlpath = '~/.kmltemp')
-as_basin = create_map_df(Asia_basin)
+## options(kmlpath = '~/.kmltemp')
+## as_basin = create_map_df(Asia_basin)
 
 WDIsearch('loans')
-fert_usage = WDI(indicator = "AG.CON.FERT.MT", country = countries, start=1960, end=2013)
+fert_usage = WDI(indicator = "AG.CON.FERT.ZS", country = countries, start=1960, end=2013)
 
 df_cereal$fertilizer = fert_usage$AG.CON.FERT.ZS
 
 WDI(indicator = "AG.CON.FERT.PT.ZS", country = countries, start=1960, end=2013)
 
-ggplot(as_basin, aes(x=long, y=lat, group=group)) + geom_polygon()
+#ggplot(as_basin, aes(x=long, y=lat, group=group)) + geom_polygon()
 
 
-temps = get_historical_temp(c('IND'), time_scale = "year")
-temps = arrange(temps, -row_number())
-temps_annual = temps$data[1:54]
-df_cereal$temps = temps_annual
+#temps = get_historical_temp(c('IND'), time_scale = "year")
+#get_historical_temp(c('IND'), time_scale = "daily")
+
+#temps = arrange(temps, -row_number())
+#temps_annual = temps$data[1:54]
+#df_cereal$temps = temps_annual
+
+## read in monthly temperature data
+## two crop seasons in india for agriculture
+## kharif: july-october and rabi:october-march
+## lets take averages for each season and average them both
+temp_1960_90 = read.xlsx('~/Downloads/tas5_1960_1990.xls', sheetIndex = 1)
+temp_1990_2012 = read.xlsx('~/Downloads/tas5_1990_2012.xls', sheetIndex = 1)
+temp_monthly = rbind(temp_1960_90, temp_1990_2012)
+temp_monthly = arrange(temp_monthly, -row_number())
+temps_kharif = temp_monthly[temp_monthly$X.Month %in% c(7,8,9,10),]
+avg_temps_kharif = colMeans(matrix(temps_kharif$Temperature..C., nrow=4))
+
+temps_rabi = temp_monthly[temp_monthly$X.Month %in% c(1,2,3),]
+avg_temps_rabi = colMeans(matrix(temps_rabi$Temperature..C., nrow=3))
+df_cereal$temp_rabi = avg_temps_rabi
+df_cereal$temp_kharif = avg_temps_kharif
+
+#avg_temps = rowMeans(cbind(avg_temps_kharif, avg_temps_rabi))
 
 precip = get_historical_precip(c("IND"), "year")
 precip = arrange(precip, -row_number())
 precip_annual = precip$data[1:54]
-df_cereal$rain = precip_annual
 
-pairs(df_cereal)
-WDIsearch('irrigation')
-methane_emission = WDI(indicator = 'EN.ATM.METH.AG.KT.CE', country=c('IN'), start=1960, end=2013)
-df_cereal$methane = methane_emission$EN.ATM.METH.AG.KT.CE
+precip_1900_2012 = read.xlsx('~/Downloads/pr5_1900_2012.xls', sheetIndex = 1)
+
+ppp = precip_1900_2012[precip_1900_2012$X.Year %in% 1959:2012,]
+ppp = arrange(ppp, -row_number())
+precip_kharif = ppp[ppp$X.Month %in% c(7,8,9,10),]
+avg_precip_kharif = colMeans(matrix(precip_kharif$Rainfall..mm., nrow=4))
+
+precip_rabi = ppp[ppp$X.Month %in% c(1,2,3),]
+avg_precip_rabi = colMeans(matrix(precip_rabi$Rainfall..mm., nrow=3))
+length(avg_precip_rabi)
+
+df_cereal$rain_rabi = avg_precip_rabi
+df_cereal$rain_kharif = avg_precip_kharif
+#avg_rainfall = rowMeans(cbind(avg_precip_kharif, avg_precip_rabi))
+get_historical_precip(c("IND"), "month")
+
+#df_cereal$rain = precip_annual
+
+#pairs(df_cereal)
+#WDIsearch('')
+#methane_emission = WDI(indicator = 'EN.ATM.NOXE.AG.KT.CE', country=c('IN'), start=1960, end=2013)
+#df_cereal$methane = methane_emission$EN.ATM.METH.AG.KT.CE
+
+
+
 
 save(df_cereal, file='df_Cereal.RData')
